ba73e5d9236f68ea2208e9aabb3e2eab36daf9e6
nexus0212 <pietrek0212@gmail.com>
2019-11-05 20:50:00 +0100

    Fixed card description

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -2,14 +2,14 @@ import { Button, Card } from 'react-bootstrap';
 import React, { Suspense } from 'react';
 
 
-const BookCard = ({ thumbnailURL, title, desciption, url }) => {
+const BookCard = ({ thumbnailURL, title, description, url }) => {
   return (
     <Suspense fallback={'loading...'}>
       <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src={thumbnailURL} />
         <Card.Body>
           <Card.Title>{title}</Card.Title>
-          <Card.Text>{desciption}</Card.Text>
+          <Card.Text>{description}</Card.Text>
           <Button href={url} variant="primary">Show more</Button>
         </Card.Body>
       </Card>
